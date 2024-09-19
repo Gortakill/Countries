be72ce913c03c3a1a196d189486148e9b1177bd2
Ruslan Tkhorenko <gortakill@gmail.com>
2024-09-19 10:11:19 +0300

    fix

--- client/src/axios/axios.ts
+++ client/src/axios/axios.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
-const instanse = axios.create({ baseURL: process.env.BASE_URL });
+const instanse = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_URL });
 
 export const getAllCountries = async () => {
     try {
         const response = await instanse.get('countries');
         return response.data;
     } catch (err) {
+        console.log(
+            'Full URL:',
+            `${process.env.NEXT_PUBLIC_BASE_URL}/countries`,
+        );
         throw new Error('Failed to load countries');
     }
 };
